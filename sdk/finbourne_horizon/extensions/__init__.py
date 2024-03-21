284349684853b6f1c22e994ec1f620c410bf4d95
Concourse <concourse@finbourne.com>
2024-03-21 10:37:08 +0000

    Updates for v2.0.115

--- sdk/finbourne_horizon/extensions/__init__.py
+++ sdk/finbourne_horizon/extensions/__init__.py
@@ -5,3 +5,14 @@ from finbourne_horizon.extensions.configuration_loaders import (
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
 )
+from finbourne_horizon.extensions.api_client import SyncApiClient
+
+__all__ = [
+    "SyncApiClientFactory",
+    "ApiClientFactory",
+    "ConfigurationLoader",
+    "SecretsFileConfigurationLoader",
+    "EnvironmentVariablesConfigurationLoader",
+    "ArgsConfigurationLoader",
+    "SyncApiClient"
+]
