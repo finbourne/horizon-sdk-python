f25b90ebfa228d6f8d4113a51927cc2b353eb2fc
Concourse <concourse@finbourne.com>
2024-10-15 09:18:22 +0000

    horizon python updates for v2.1.228
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/finbourne_horizon/configuration.py
+++ sdk/finbourne_horizon/configuration.py
@@ -445,7 +445,7 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.1.889\n"\
+               "Version of the API: 0.1.893\n"\
                "SDK Package Version: {package_version}".\
                format(env=sys.platform, pyversion=sys.version, package_version=package_version)
 
